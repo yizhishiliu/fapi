7915785b4e73e225a631d482c9995a1049fd4197
yizhishiliu <mingdajiang5@gmail.com>
2024-11-11 22:39:11 +0800

    后端初始化

---
+++ fapi-backend/src/main/java/com/shiliu/fapi/mapper/UserMapper.java
@@ -0,0 +1,17 @@
+package com.shiliu.fapi.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.shiliu.fapi.model.entity.User;
+
+/**
+ * 用户数据库操作
+ *
+ * @author <a href="https://github.com/yizhishiliu">一之十六</a>
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}
+
+
+
+
