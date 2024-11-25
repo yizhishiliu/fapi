3cb1aaae29a0baaab1b8f5e1d584f64e2f96fe32
yizhishiliu <mingdajiang5@gmail.com>
2024-11-25 22:00:00 +0800

    权限管理

--- fapi-frontend/src/access.ts
+++ fapi-frontend/src/access.ts
@@ -1,9 +1,12 @@
 /**
  * @see https://umijs.org/docs/max/access#access
  * */
-export default function access(initialState: { currentUser?: API.CurrentUser } | undefined) {
-  const { currentUser } = initialState ?? {};
+export default function access(initialState: InitialState) {
+  const { loginUser } = initialState ?? {};
   return {
-    canAdmin: currentUser && currentUser.access === 'admin',
+    // 普通用户权限，登录即可访问
+    canUser: loginUser,
+    // 管理员权限
+    canAdmin: loginUser?.userRole === 'admin',
   };
 }
