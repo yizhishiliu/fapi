738f3e865bfbfee3f2fcb15f3d360e8dff3a1877
yizhishiliu <mingdajiang5@gmail.com>
2024-11-30 20:55:52 +0800

    API接口

---
+++ fapi-interface/src/main/java/com/shiliu/fapiinterface/FapiInterfaceApplication.java
@@ -0,0 +1,13 @@
+package com.shiliu.fapiinterface;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FapiInterfaceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FapiInterfaceApplication.class, args);
+    }
+
+}
---
+++ fapi-interface/src/test/java/com/shiliu/fapiinterface/FapiInterfaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shiliu.fapiinterface;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FapiInterfaceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
