353837557b9ba0d79cfa96d63665b7f4514c1a55
yizhishiliu <mingdajiang5@gmail.com>
2024-12-01 23:21:04 +0800

    fapi-client-sdk测试

--- fapi-interface/src/test/java/com/shiliu/fapiinterface/FapiInterfaceApplicationTests.java
+++ fapi-interface/src/test/java/com/shiliu/fapiinterface/FapiInterfaceApplicationTests.java
@@ -1,5 +1,7 @@
 package com.shiliu.fapiinterface;
 
+import com.shiliu.fapiclientsdk.client.FApiClient;
+import com.shiliu.fapiclientsdk.model.User;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +10,11 @@ class FapiInterfaceApplicationTests {
 
     @Test
     void contextLoads() {
+        FApiClient client = new FApiClient("shiliu", "aaAA1234");
+        User user = new User();
+        user.setUserName("shiliu");
+        String result = client.helloByPost2(user);
+        System.out.println(result);
     }
 
 }
