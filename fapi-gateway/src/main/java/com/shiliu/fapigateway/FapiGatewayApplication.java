2cbf598e6f64aecff9b81d2311e5c9d6fbadb482
yizhishiliu <mingdajiang5@gmail.com>
2024-12-15 18:40:56 +0800

    初始化网关项目

---
+++ fapi-gateway/src/main/java/com/shiliu/fapigateway/FapiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.shiliu.fapigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FapiGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FapiGatewayApplication.class, args);
+    }
+
+}
---
+++ fapi-gateway/src/test/java/com/shiliu/fapigateway/FapiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shiliu.fapigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FapiGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
